5b59220b15b98dbdb7ba6c7001553d5c387f6615
Mirko Hartung <6hartung@informatik.uni-hamburg.de>
2022-02-18 13:13:02 +0100

    rework automount functions

--- udiskie/automount.py
+++ udiskie/automount.py
@@ -25,14 +25,15 @@ class AutoMounter(DaemonBase):
     >>> automounter.activate()
     """
 
-    def __init__(self, mounter, automount=True):
+    def __init__(self, mounter, automount=True, virtmount=False):
         """Store mounter as member variable."""
         self._mounter = mounter
         self._automount = automount
+        self._virtmount = virtmount
         self.events = {
-            'device_changed': self.device_changed_virt,
-            'device_added': self.auto_add_virt,
-            'media_added': self.auto_add_virt,
+            'device_changed': self.device_changed,
+            'device_added': self.auto_add,
+            'media_added': self.auto_add,
             'device_removed': self.device_removed,
         }
 
@@ -42,6 +43,12 @@ class AutoMounter(DaemonBase):
     def toggle_on(self):
         self._automount = not self._automount
 
+    def toggle_virtmount(self):
+        self._virtmount = not self._virtmount
+
+    def is_virtmount_on(self):
+        return self._virtmount
+
     def device_changed(self, old_state, new_state):
         """Mount newly mountable devices."""
         # udisks2 sometimes adds empty devices and later updates them - which
@@ -49,24 +56,17 @@ class AutoMounter(DaemonBase):
         if (self._mounter.is_addable(new_state)
                 and not self._mounter.is_addable(old_state)
                 and not self._mounter.is_removable(old_state)):
-            self.auto_add(new_state)
+            if self._virtmount:
+                self.auto_add_virt(new_state)
+            else:
+                self.auto_add(new_state)
 
     @run_bg
     def auto_add(self, device):
-        return self._mounter.auto_add(device, automount=self._automount)
-
-    def device_changed_virt(self, old_state, new_state):
-        """Mount newly mountable devices."""
-        # udisks2 sometimes adds empty devices and later updates them - which
-        # makes is_external become true at a time later than device_added:
-        if (self._mounter.is_addable(new_state)
-                and not self._mounter.is_addable(old_state)
-                and not self._mounter.is_removable(old_state)):
-            self.auto_add_virt(new_state)
-
-    @run_bg
-    def auto_add_virt(self, device):
-        return self._mounter.auto_add_virt(device, automount=self._automount)
+        if self._virtmount:
+            return self._mounter.auto_add_virt(device, automount=self._automount)
+        else:
+            return self._mounter.auto_add(device, automount=self._automount)
 
     def device_removed(self, device):
         print("Device removed! %s Filesystem?: %s" % (device, device.is_filesystem))
