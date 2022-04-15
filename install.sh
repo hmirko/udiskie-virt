5c11acdae3500fd404959c8c4b0c4cffb7d76cd9
Mirko Hartung <6hartung@informatik.uni-hamburg.de>
2022-04-15 11:51:23 +0200

    improve install script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+##!/bin/bash -ex
 
 # Dependecies
+echo "Installing dependencies."
+sudo apt get update
 sudo apt install\
      udisks2\
      libguestfs-dev\
@@ -10,8 +13,11 @@ sudo apt install\
      python3-guestfs\
      tint2
 
+echo ""
+echo ""
 
 #  Udiskie
+echo "Installing udiskie-virt."
 
 AUTO_DIR=~/.config/autostart
 SCRIPT=$0
@@ -35,14 +41,15 @@ if [ -d $AUTO_DIR ]; then
     cp "$SCRIPT_DIR"/udiskie-virt.desktop "$AUTO_DIR"
 fi
 
-
-echo "Installing udiskie-virt."
 pip3 install -e $REPO_DIR
+
+echo ""
+echo ""
 
 # Group / Permission
 echo "Creating group virtmount."
 sudo groupadd virtmount
-echo "Addint $USER to virtmount. You will have to start a new session for this to take effect."
+echo "Add $USER to virtmount. You will have to start a new session for this to take effect."
 sudo usermod -a -G virtmount $USER
 
 echo "Creating location for virtual mounts."
@@ -55,14 +62,76 @@ sudo chmod g+w /media/virtmount
 echo "Giving users permissions to use the kernel image."
 sudo chmod 755 /boot/vmlinuz*
 
-
 echo "Installing udev rule."
 # https://unix.stackexchange.com/questions/333697/etc-udev-rules-d-vs-lib-udev-rules-d-which-to-use-and-why
-sudo cp "${REPO_DIR}/script/99-external-media.rules" /udev/rules.d
+sudo cp "${REPO_DIR}/script/99-external-media.rules" /etc/udev/rules.d
 sudo udevadm control --reload-rules
 sudo udevadm trigger
 
+echo ""
+echo ""
 
-# Checking
-echo "Checking correct install of libguestfs."
-libguestfs-test-tool
+# Disable automounting of known desktop environments
+echo "Disable automounting of known desktop environments."
+
+if [ $DESKTOP_SESSION != "" ]
+then
+    desktop_environment="$DESKTOP_SESSION"
+elif [ $XDG_CURRENT_DESKTOP != "" ]
+then
+    desktop_environment="$XDG_CURRENT_DESKTOP"
+elif [ $GDMSESSION != "" ]
+then
+    desktop_environment="$GDMSESSION"
+    echo ""
+fi
+
+if [ $desktop_environment != "" ]
+then
+    desktop_environment=$(echo $desktop_environment | tr [:upper:] [:lower:])
+    echo "Detected ${desktop_environment}."
+fi
+
+case $desktop_environment in
+    "ubuntu" | "ubuntu:gnome" | "ubuntu:GNOME")
+	gsettings set org.gnome.desktop.media-handling automount false
+
+
+	;;
+    "MATE" | "mate")
+	gsettings set org.mate.media-handling automount false
+    ;;
+
+    "plasma" | "kde")
+	# manipulate INI-format files
+	sudo apt install crudini
+	crudini --set"$HOME/.config/kded5rc" "Module-device_automounter" "autoload" "false"
+	;;
+    "lubuntu" | "lxqt")
+	# manipulate INI-format files
+	sudo apt install crudini
+	crudini --set "$HOME/.config/pcmanfm-qt/lxqt/settings.conf" "Volume" "autorun" "false"
+	crudini --set "$HOME/.config/pcmanfm-qt/lxqt/settings.conf" "Volume" "mount_on_startup" "false"
+	crudini --set "$HOME/.config/pcmanfm-qt/lxqt/settings.conf" "Volume" "mount_removable" "false"
+	;;
+    "xubuntu" | "xfce")
+	xfconf-query -c thunar-volman -np "/automount-media/enabled" -t "bool" -s "false"
+	xfconf-query -c thunar-volman -np "/autoburn/enabled" -t "bool" -s "false"
+	xfconf-query -c thunar-volman -np "/autobrowse/enabled" -t "bool" -s "false"
+	xfconf-query -c thunar-volman -np "/autoipod/enabled" -t "bool" -s "false"
+	xfconf-query -c thunar-volman -np "/automount-drives/enabled" -t "bool" -s "false"
+	xfconf-query -c thunar-volman -np "/autoopen/enabled" -t "bool" -s "false"
+	xfconf-query -c thunar-volman -np "/autophoto/enabled" -t "bool" -s "false"
+	xfconf-query -c thunar-volman -np "/autoplay-audio-cds/enabled" -t "bool" -s "false"
+	xfconf-query -c thunar-volman -np "/autoplay-video-cds/enabled" -t "bool" -s "false"
+	xfconf-query -c thunar-volman -np "/autorun/enabled" -t "bool" -s "false"
+	;;
+    "budgie:gnome" | "Budgie:GNOME" | "budgie-desktop")
+	gsettings set org.gnome.desktop.media-handling automount false
+	gsettings set org.cinnamon.desktop.media-handling automount false
+	;;
+
+  *)
+	echo "No solution for $desktop_environment found. Please deactivate automounting manually"
+    ;;
+esac
